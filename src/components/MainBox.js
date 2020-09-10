cbafd3326c1a0d53fab00f112fc0b37e88351961
Francis Bonacic <francisbonacic@icloud.com>
2020-09-09 21:53:12 -0700

    finished

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -6,13 +6,22 @@ const MainBox = (props) => {
 
     console.log(props.dailyPhoto.copyright)
 
+    const nasaData = props.dailyPhoto
+
 
 
     return (
 
     
     <div className='mainbox'>
+
         <h1>NASA DAILY</h1>
+
+    <h2>{nasaData.title}</h2>
+    <h3>Date: {nasaData.date}</h3>
+    <p className='about'> About: {nasaData.explanation}</p>
+    <img alt = 'deep space' src = {nasaData.url}/>
+    <p className='copyright'>Copyright: {nasaData.copyright}</p>
     </div>
 
 
